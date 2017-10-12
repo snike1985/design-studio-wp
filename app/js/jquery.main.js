3989b99164722ec5173e357bfe4f8dd4c5939c02
max_tkhorenko <max.tkh.ua@gmial.com>
2017-10-12 10:41:48 +0300

    request fix

--- app/js/jquery.main.js
+++ app/js/jquery.main.js
@@ -1,4 +1,35 @@
-"use strict";
-( function(){
+$(function () {
 
-} )();
+    $.each( $( '.message-field' ), function() {
+        new MessageHigh( $( this ) );
+    } );
+
+} );
+
+var MessageHigh = function ( obj ) {
+
+    var _obj = obj,
+        _message = _obj.find( '.contact-form__message' ),
+        _messageText = _obj.find( '.message-field__text' ),
+        _messageHeight = _obj.find( '.message-field__height' );
+
+    var _onEvents = function() {
+
+            _obj.on( {
+                'keydown' : function() {
+
+                    _messageText.html( _message.val() + '__' );
+                    _message.css( 'height', _messageText.height() + 25 );
+                    _messageHeight.css( 'height', _messageText.height() );
+
+                }
+            } );
+
+        },
+        _init = function() {
+            _onEvents();
+        };
+
+    _init();
+
+};
